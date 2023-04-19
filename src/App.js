d3a1d8303452220935a0e072c66b64b5d7a52ed1
Tushar <reddygtvs@gmail.com>
2023-04-19 19:38:53 +0530

    correct routes for flask backend

--- src/App.js
+++ src/App.js
@@ -5,16 +5,22 @@ import axios from "axios";
 const App = () => {
   const [selectedFile, setSelectedFile] = useState(null);
   const [prediction, setPrediction] = useState(null);
+  //const [image, setImage] = useState(null);
 
   const handleFileChange = (e) => {
     setSelectedFile(e.target.files[0]);
+    //setImage(e.target.files[0]);
   };
 
   const handlePredict = () => {
     const formData = new FormData();
     formData.append("image", selectedFile);
     axios
-      .post("/predict", formData)
+      .post("http://127.0.0.1:5000/predict", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      })
       .then((res) => {
         setPrediction(res.data.prediction);
       })
@@ -27,7 +33,13 @@ const App = () => {
     <div className="App">
       <h1> Haemoprotozoan Disease Prediction</h1>
       <h3> Upload your image here </h3>
-      <input type="file" onChange={handleFileChange} />
+
+      <input
+        type="file"
+        accept="image/*"
+        name="image"
+        onChange={handleFileChange}
+      />
       <button onClick={handlePredict}>Predict</button>
       {prediction && (
         <div>
