2c5cb15be73c84099ebc5e7892a7d2838bf3446a
Tushar <reddygtvs@gmail.com>
2023-04-19 15:11:19 +0530

    basic frontend setup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
 import "./App.css";
+import React, { useState } from "react";
+import axios from "axios";
 
-function App() {
+const App = () => {
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [prediction, setPrediction] = useState(null);
+
+  const handleFileChange = (e) => {
+    setSelectedFile(e.target.files[0]);
+  };
+
+  const handlePredict = () => {
+    const formData = new FormData();
+    formData.append("image", selectedFile);
+    axios
+      .post("/predict", formData)
+      .then((res) => {
+        setPrediction(res.data.prediction);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <h1> Haemoprotozoan Disease Prediction</h1>
+      <h3> Upload your image here </h3>
+      <input type="file" onChange={handleFileChange} />
+      <button onClick={handlePredict}>Predict</button>
+      {prediction && (
+        <div>
+          <h2>Prediction Result:</h2>
+          <p>{prediction}</p>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
